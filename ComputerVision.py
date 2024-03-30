9a36e26847bd8d70845cbf2939ec7b073c3d05df
mdb326 <111697762+mdb326@users.noreply.github.com>
2024-03-30 13:04:49 -0400

    Add files via upload

---
+++ ComputerVision.py
@@ -0,0 +1,51 @@
+import pyb # Import module for board related functions
+import sensor # Import the module for sensor related functions
+import image # Import module containing machine vision algorithms
+import time # Import module for tracking elapsed time
+
+sensor.reset() # Resets the sensor
+sensor.set_pixformat(sensor.RGB565) # Sets the sensor to RGB
+sensor.set_framesize(sensor.QVGA) # Sets the resolution to 320x240 px
+sensor.set_vflip(False) # Flips the image vertically
+sensor.set_hmirror(False) # Mirrors the image horizontally
+sensor.skip_frames(time = 2000) # Skip some frames to let the image stabilize
+
+# Define the min/max LAB values we're looking for
+thresholdsApple = (24, 60, 32, 54, 0, 42)
+thresholdsBanana = (34, 100, -29, 27, 22, 82)
+
+ledRed = pyb.LED(1) # Initiates the red led
+ledGreen = pyb.LED(2) # Initiates the green led
+
+clock = time.clock() # Instantiates a clock object
+
+while(True):
+    clock.tick() # Advances the clock
+    img = sensor.snapshot() # Takes a snapshot and saves it in memory
+
+    # Find blobs with a minimal area of 50x50 = 2500 px
+    # Overlapping blobs will be merged
+    blobs = img.find_blobs([thresholdsApple, thresholdsBanana], area_threshold=2500, merge=True)
+
+    # Draw blobs
+    for blob in blobs:
+        # Draw a rectangle where the blob was found
+        img.draw_rectangle(blob.rect(), color=(0,255,0))
+        # Draw a cross in the middle of the blob
+        img.draw_cross(blob.cx(), blob.cy(), color=(0,255,0))
+        print(blob.cx()," ", blob.cy())
+        print(blob.rect())
+        #print(blob.rect()[2] * blob.rect()[3])
+
+
+    # Turn on green LED if a blob was found
+    if len(blobs) > 0:
+        ledGreen.on()
+        ledRed.off()
+    else:
+    # Turn the red LED on if no blob was found
+        ledGreen.off()
+        ledRed.on()
+
+    pyb.delay(50) # Pauses the execution for 50ms
+    #print(clock.fps()) # Prints the framerate to the serial console
